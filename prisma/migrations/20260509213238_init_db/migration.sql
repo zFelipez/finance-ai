87cb4247cf45883b38c43c2cf580d414a5b45cfa
zFelipez <felipefesilva19@gmail.com>
2026-05-09 18:34:29 -0300

    feat(db): add transaction table migration

---
+++ prisma/migrations/20260509213238_init_db/migration.sql
@@ -0,0 +1,23 @@
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('DEPOSIT', 'EXPENSE', 'INVESTMENT');
+
+-- CreateEnum
+CREATE TYPE "TransactionCategory" AS ENUM ('HOUSING', 'FOOD', 'TRANSPORTATION', 'ENTERTAINMENT', 'UTILITY', 'HEALTH', 'SALARY', 'EDUCATION', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "TransactionPaymentMethod" AS ENUM ('CASH', 'CREDIT_CARD', 'DEBIT_CARD', 'BANK_TRANSFER', 'MOBILE_PAYMENT', 'OTHER');
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "type" "TransactionType" NOT NULL,
+    "amount" DECIMAL(10,2) NOT NULL,
+    "category" "TransactionCategory" NOT NULL,
+    "paymentMethod" "TransactionPaymentMethod" NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
