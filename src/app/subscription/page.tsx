e8fcb55639b4d6b0504bddf75512e4ca7c41e0e5
zFelipez <felipefesilva19@gmail.com>
2026-05-09 18:14:32 -0300

    chore: install prisma dep

---
+++ src/app/subscription/page.tsx
@@ -0,0 +1,11 @@
+
+
+
+export  default function SubscriptionPage() {
+    return (
+        <div>
+            <h1>Subscription Page</h1>
+            <p>Manage your subscription here.</p>
+        </div>
+    );
+}
