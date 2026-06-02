8f7d3e32829d3b6d86fcad9e982365fee55a523e
zFelipez <felipefesilva19@gmail.com>
2026-06-02 10:31:55 -0300

    fix: upsert transaction dialog schema type errors

--- src/_components/upsert-transaction-dialog.tsx
+++ src/_components/upsert-transaction-dialog.tsx
@@ -55,16 +55,19 @@ const formSchema = z.object({
   name: z.string().trim().min(1, {
     message: "O nome é obrigatório.",
   }),
-  amount: z.number("O valor é obrigatório.").positive({
+  amount: z.number({ required_error: "O valor é obrigatório." }).positive({
     message: "O valor deve ser positivo.",
   }),
-  type: z.nativeEnum(TransactionType, "O tipo é obrigatório."),
-  category: z.nativeEnum(TransactionCategory, "A categoria é obrigatória."),
-  paymentMethod: z.nativeEnum(
-    TransactionPaymentMethod,
-    "O método de pagamento é obrigatório.",
-  ),
-  date: z.date("A data é obrigatória."),
+  type: z.nativeEnum(TransactionType, {
+    required_error: "O tipo é obrigatório.",
+  }),
+  category: z.nativeEnum(TransactionCategory, {
+    required_error: "A categoria é obrigatória.",
+  }),
+  paymentMethod: z.nativeEnum(TransactionPaymentMethod, {
+    required_error: "O método de pagamento é obrigatório.",
+  }),
+  date: z.date({ required_error: "A data é obrigatória." }),
 });
 
 export type formSchemaType = z.infer<typeof formSchema>;
