68c9ec8b83ac5373dd7f3b6ef05fbf93316870b7
zFelipez <felipefesilva19@gmail.com>
2026-06-02 11:33:50 -0300

    fix: adjust stripe session success and cancel urls redirection

--- src/app/subscription/_actions/create-stripe-checkout/index.ts
+++ src/app/subscription/_actions/create-stripe-checkout/index.ts
@@ -1,37 +1,38 @@
 "use server";
 
 import { auth } from "@clerk/nextjs/server";
-
 import Stripe from "stripe";
 
-export async function createStripeCheckout() {
+export const createStripeCheckout = async () => {
   const { userId } = await auth();
-
   if (!userId) {
-    throw new Error("User not authenticated");
+    throw new Error("Unauthorized");
   }
-
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("Stripe secret key not found");
+  }
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2024-10-28.acacia",
   });
-
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "subscription",
-    success_url: `${process.env.NEXT_APP_URL}`,
-    cancel_url: `${process.env.NEXT_APP_URL}/subscription`,
-    line_items: [
-      {
-        price: process.env.STRIPE_PREMIUM_PLAN_PRICE_ID!,
-        quantity: 1,
-      },
-    ],
+    success_url: process.env.NEXT_APP_URL,
+    cancel_url: process.env.NEXT_APP_URL,
+    metadata: {
+      clerk_user_id: userId,
+    },
     subscription_data: {
       metadata: {
         clerk_user_id: userId,
       },
     },
+    line_items: [
+      {
+        price: process.env.STRIPE_PREMIUM_PLAN_PRICE_ID,
+        quantity: 1,
+      },
+    ],
   });
-
   return { sessionId: session.id };
-}
+};
